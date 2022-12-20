5cadda0c43b8b4c24ac1798949c7dc582f9046bc
DongwookKwak <ume1838@gmail.com>
2022-12-20 12:01:21 +0900

    MIT Mode, Timer/GPIO Interrupt, Position Control

---
+++ Src/main.c
@@ -0,0 +1,556 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "CANOpen/CANOpen.h"
+#include <math.h>
+#include "CANOpen/CANOpen_hw_appl.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ CAN_HandleTypeDef hcan1;
+
+TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_CAN1_Init(void);
+static void MX_TIM1_Init(void);
+static void MX_TIM2_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+#define P_MIN -12.5f
+#define P_MAX 12.5f
+#define V_MIN -45.0f
+#define V_MAX 45.0f
+#define Kp_MIN 0.0f
+#define Kp_MAX 500.0f
+#define Kd_MIN 0.0f
+#define Kd_MAX 5.0f
+#define T_MIN -15.0f
+#define T_MAX 15.0f
+
+//initialize value
+float torque=0.0f;
+float position=0.0f;
+float velocity=0.0f;
+
+float p_in=0.0f;
+float v_in=0.0f;
+float kp_in = 50.0f;
+float kd_in = 1.0f;
+float t_in =0.0f;
+
+uint8_t buffer[8];
+
+//when sending packet, all the numbers should be converted into integer numbers
+int float_to_uint(float x, float x_min, float x_max, int bits){
+    /// Converts a float to an unsigned int, given range and number of bits ///
+    float span = x_max - x_min;
+    float offset = x_min;
+    return (unsigned int) ((x-offset)*((float)((1<<bits)-1))/span);
+    }
+
+//send routine
+void pack_cmd(uint8_t* buffer, float p_des, float v_des, float kp, float kd, float t_ff){
+  
+  p_des = fminf(fmaxf(P_MIN, p_des), P_MAX);   
+  v_des = fminf(fmaxf(V_MIN, v_des), V_MAX);
+  kp = fminf(fmaxf(Kp_MIN, kp), Kp_MAX);
+  kd = fminf(fmaxf(Kd_MIN, kd), Kd_MAX);
+  t_ff = fminf(fmaxf(T_MIN, t_ff), T_MAX);
+  /// convert floats to unsigned ints ///
+  unsigned int p_int = float_to_uint(p_des, P_MIN, P_MAX, 16);
+  unsigned int v_int = float_to_uint(v_des, V_MIN, V_MAX, 12);
+  unsigned int kp_int = float_to_uint(kp, Kp_MIN, Kp_MAX, 12);
+  unsigned int kd_int = float_to_uint(kd, Kd_MIN, Kd_MAX, 12);
+  unsigned int t_int = float_to_uint(t_ff, T_MIN, T_MAX, 12);
+  /// pack ints into the can buffer ///
+  buffer[0] = p_int>>8; // Position 8 higher
+  buffer[1] = p_int&0xFF; // Position 8 lower
+  buffer[2] = v_int>>4; // Speed 8 higher
+  buffer[3] = ((v_int&0xF)<<4)|(kp_int>>8); //Speed 4 bit lower KP 4bit higher
+  buffer[4] = kp_int&0xFF; // KP 8 bit lower
+  buffer[5] = kd_int>>4; // Kd 8 bit higher
+  buffer[6] = ((kd_int&0xF)<<4)|(kp_int>>8); //KP 4 bit lower torque 4 bit higher
+  buffer[7] = t_int&0xFF; // torque 4 bit lower
+  
+}
+
+float uint_to_float(int x_int, float x_min, float x_max, int bits){ 
+    /// converts unsigned int to float, given range and number of bits ///
+    float span = x_max - x_min;
+    float offset = x_min;
+    return ((float)x_int)*span/((float)((1<<bits)-1)) + offset;
+    }
+
+//receive routine
+void unpack_reply(uint8_t* buffer){
+  /// unpack ints from can buffer ///
+  int id = buffer[0]; //??ID
+  int p_int = (buffer[1]<<8)|buffer[2]; //Motor position data
+  int v_int = (buffer[3]<<4)|(buffer[4]>>4); // Motor speed data
+  int t_int = ((buffer[4]&0xF)<<8)|buffer[5]; // Motor torque data
+  /// convert ints to floats ///
+  float p = uint_to_float(p_int, P_MIN, P_MAX, 16);
+  float v = uint_to_float(v_int, V_MIN, V_MAX, 12);
+  float t = uint_to_float(t_int, T_MIN, T_MAX, 12);
+  if(id == 1){
+    position = p; //Read the corresponding data according to the ID code
+    velocity = v;
+    torque = t;
+  }
+}
+
+CAN_RxHeaderTypeDef   Rx0Header;
+uint8_t               Rx0Data[8];
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan){
+  if(HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &Rx0Header, Rx0Data) == HAL_OK){
+    CANOpen_addRxBuffer(Rx0Header.StdId, Rx0Data);
+    unpack_reply(Rx0Data);
+  }  
+}
+
+void pull_up(){
+  p_in=position;
+  v_in=0.0f;
+  kp_in = 0.0f;
+  kd_in = 0.0f;
+  t_in =0.0f;
+  
+  pack_cmd(buffer, p_in, v_in, kp_in, kd_in, t_in);
+  CANOpen_sendFrame(1, buffer, 8);
+}
+
+void enter_motor_mode(){
+  buffer[0]=0xFF;
+  buffer[1]=0xFF;
+  buffer[2]=0xFF;
+  buffer[3]=0xFF;
+  buffer[4]=0xFF;
+  buffer[5]=0xFF;
+  buffer[6]=0xFF;
+  buffer[7]=0xFC;
+
+  CANOpen_sendFrame(1, buffer, 8);
+}
+
+void exit_motor_mode(){
+  buffer[0]=0xFF;
+  buffer[1]=0xFF;
+  buffer[2]=0xFF;
+  buffer[3]=0xFF;
+  buffer[4]=0xFF;
+  buffer[5]=0xFF;
+  buffer[6]=0xFF;
+  buffer[7]=0xFD;
+
+  CANOpen_sendFrame(1, buffer, 8);
+}
+
+void set_zero(){
+  buffer[0]=0xFF;
+  buffer[1]=0xFF;
+  buffer[2]=0xFF;
+  buffer[3]=0xFF;
+  buffer[4]=0xFF;
+  buffer[5]=0xFF;
+  buffer[6]=0xFF;
+  buffer[7]=0xFE;
+  
+  CANOpen_sendFrame(1, buffer, 8);
+}
+
+uint8_t flag=0;
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
+  if(htim == &htim1){ // 10kHz Loop
+    CANOpen_timerLoop();
+  }else if(htim == &htim2){ // 1kHz main Loop
+     pack_cmd(buffer, p_in, v_in, kp_in, kd_in, t_in);
+      
+     CANOpen_sendFrame(1, buffer, 8);
+    
+  }
+}
+
+
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+  if(GPIO_Pin == GPIO_PIN_13){
+    if(flag==0){
+      set_zero();                                         //set origin
+      //exit_motor_mode();
+    }
+    else if(flag==1){
+      enter_motor_mode();                                 //motor mode enter
+    }
+    else if(flag==2){
+      // [[ Timer1 Init ]]
+      // Start Timer1 to check timeout of CANOpen response (10kHz)
+      HAL_TIM_Base_Start_IT(&htim1);
+      
+      // [[ Start main loop ]]
+      HAL_TIM_Base_Start_IT(&htim2);
+      }
+    else if(flag==3){
+      pull_up();                                         //0 set (parking gear)
+    }
+    else if(flag==4){ 
+      HAL_TIM_Base_Stop_IT(&htim2);
+      HAL_TIM_Base_Stop_IT(&htim1);
+      exit_motor_mode();                                 //exit
+      }
+    else if(flag==27){
+      float delta = 0.1f;
+      p_in = p_in+delta;
+    }
+  }
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_CAN1_Init();
+  MX_TIM1_Init();
+  MX_TIM2_Init();
+  /* USER CODE BEGIN 2 */
+  
+  // [[ CAN Init ]]
+  // This mcu is CANOpen Master. So it should receive all can frame.
+  CAN_FilterTypeDef sFilterConfig0;
+  sFilterConfig0.FilterBank = 0;
+  sFilterConfig0.FilterMode = CAN_FILTERMODE_IDMASK;
+  sFilterConfig0.FilterScale = CAN_FILTERSCALE_16BIT;
+  sFilterConfig0.FilterIdLow = 0x0000;
+  sFilterConfig0.FilterMaskIdLow = 0x0000;
+  sFilterConfig0.FilterIdHigh = 0x0000;
+  sFilterConfig0.FilterMaskIdHigh = 0x0000;
+  sFilterConfig0.FilterFIFOAssignment = CAN_RX_FIFO0;
+  sFilterConfig0.FilterActivation = ENABLE;
+  HAL_CAN_ConfigFilter(&hcan1, &sFilterConfig0);
+
+  HAL_CAN_Start(&hcan1);
+  HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING);
+  
+  
+  
+  
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    //CANOpen_writeOD_int16(0x01, 0x6071, 0x00, torque, 10);
+    //HAL_Delay(10);
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 120;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief CAN1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_CAN1_Init(void)
+{
+
+  /* USER CODE BEGIN CAN1_Init 0 */
+
+  /* USER CODE END CAN1_Init 0 */
+
+  /* USER CODE BEGIN CAN1_Init 1 */
+
+  /* USER CODE END CAN1_Init 1 */
+  hcan1.Instance = CAN1;
+  hcan1.Init.Prescaler = 3;
+  hcan1.Init.Mode = CAN_MODE_NORMAL;
+  hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
+  hcan1.Init.TimeSeg1 = CAN_BS1_8TQ;
+  hcan1.Init.TimeSeg2 = CAN_BS2_1TQ;
+  hcan1.Init.TimeTriggeredMode = DISABLE;
+  hcan1.Init.AutoBusOff = DISABLE;
+  hcan1.Init.AutoWakeUp = DISABLE;
+  hcan1.Init.AutoRetransmission = ENABLE;
+  hcan1.Init.ReceiveFifoLocked = DISABLE;
+  hcan1.Init.TransmitFifoPriority = DISABLE;
+  if (HAL_CAN_Init(&hcan1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN CAN1_Init 2 */
+
+  /* USER CODE END CAN1_Init 2 */
+
+}
+
+/**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 1;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 5999;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 0;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 59999;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
