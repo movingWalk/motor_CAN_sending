b1b71ef759064f443c63971048129f0ad23ff903
DongwookKwak <ume1838@gmail.com>
2022-12-23 10:05:16 +0900

    Servo Mode id 68

--- Src/main.c
+++ Src/main.c
@@ -61,7 +61,6 @@ typedef enum {
  CAN_HandleTypeDef hcan1;
 
 TIM_HandleTypeDef htim1;
-TIM_HandleTypeDef htim2;
 
 /* USER CODE BEGIN PV */
 
@@ -72,7 +71,6 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_CAN1_Init(void);
 static void MX_TIM1_Init(void);
-static void MX_TIM2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -90,11 +88,6 @@ float motor_cur;
 int8_t motor_temp;
 int8_t motor_error;
 
-void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan){
-  if(HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &Rx0Header, Rx0Data) == HAL_OK){
-    CANOpen_addRxBuffer(Rx0Header.ExtId, Rx0Data);
-  }  
-}
 
 //can receive protocol
 
@@ -110,6 +103,11 @@ void motor_receive(uint8_t* rx_message)
   motor_error= rx_message[7] ;//motor error mode
 }
 
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan){
+  if(HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &Rx0Header, Rx0Data) == HAL_OK){
+    motor_receive(Rx0Data);
+  }  
+}
 
 //**********************************sending***************************//
 uint8_t buffer[8];
@@ -206,7 +204,7 @@ void enter_motor_mode(){
   buffer[5]=0xFF;
   buffer[6]=0xFF;
   buffer[7]=0xFC;
-  uint8_t id = 68;
+  uint8_t id = 0x68;
   sendFrameEXTId(id, buffer, 8);
 }
 
@@ -219,19 +217,17 @@ void exit_motor_mode(){
   buffer[5]=0xFF;
   buffer[6]=0xFF;
   buffer[7]=0xFD;
-  uint8_t id = 68;
+  uint8_t id = 0x68;
   sendFrameEXTId( id, buffer, 8);
 }
 
 
 //***************************loop*****************************//
 float rpm=1000.0f;
-uint8_t controller_id=68;
+uint8_t controller_id=0x68;
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
-  if(htim == &htim1){ // 10kHz Loop
-    CANOpen_timerLoop();
-  }else if(htim == &htim2){ // 1kHz main Loop
+  if(htim == &htim1){ 
     comm_can_set_rpm(controller_id, rpm);
   }
 }
@@ -242,15 +238,16 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
   if(GPIO_Pin == GPIO_PIN_13){
     if(flag==14){
       // [[ Timer1 Init ]]
-      // Start Timer1 to check timeout of CANOpen response (10kHz)
+      // [[ Start main loop ]] (1kHz)
       enter_motor_mode();                                 //motor mode enter
-      HAL_TIM_Base_Start_IT(&htim1);
-      // [[ Start main loop ]]
-      HAL_TIM_Base_Start_IT(&htim2);
+      
+      
+      //HAL_TIM_Base_Start_IT(&htim1);
+      
     }
     else if(flag==82){
-      HAL_TIM_Base_Stop_IT(&htim2);
-      HAL_TIM_Base_Stop_IT(&htim1);
+      
+      //HAL_TIM_Base_Stop_IT(&htim1);
       exit_motor_mode();                                 //exit
 
     }
@@ -292,7 +289,6 @@ int main(void)
   MX_GPIO_Init();
   MX_CAN1_Init();
   MX_TIM1_Init();
-  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
   
   // [[ CAN Init ]]
@@ -435,7 +431,7 @@ static void MX_TIM1_Init(void)
   htim1.Instance = TIM1;
   htim1.Init.Prescaler = 1;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 5999;
+  htim1.Init.Period = 59999;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
@@ -457,51 +453,6 @@ static void MX_TIM1_Init(void)
   /* USER CODE BEGIN TIM1_Init 2 */
 
   /* USER CODE END TIM1_Init 2 */
-
-}
-
-/**
-  * @brief TIM2 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM2_Init(void)
-{
-
-  /* USER CODE BEGIN TIM2_Init 0 */
-
-  /* USER CODE END TIM2_Init 0 */
-
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-
-  /* USER CODE BEGIN TIM2_Init 1 */
-
-  /* USER CODE END TIM2_Init 1 */
-  htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 0;
-  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 59999;
-  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM2_Init 2 */
-
-  /* USER CODE END TIM2_Init 2 */
 
 }
 
